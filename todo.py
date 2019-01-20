fc66d59e7d7b887623220eea96ef92c1599f7ff9
yamini-harsola <yaminiharsola1710@gmail.com>
2019-01-20 19:35:17 +0530

    new_commit

--- todo.py
+++ todo.py
@@ -1,12 +1,14 @@
 import sqlite3
-from bottle import route, run
+from bottle import route, run, debug, template
 
 @route('/todo')
+@route('/todo-new-page')
 def todo_list():
     conn = sqlite3.connect('todo.db')
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
-    return str(result)
-
-run()
+    c.close()
+    return template('make_table', rows=result)
+debug(True)
+run(reloader=True)
