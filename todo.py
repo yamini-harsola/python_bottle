8c69375e31c84d05d26501b72bf4e01a34b8c715
yamini-harsola <yaminiharsola1710@gmail.com>
2019-01-21 00:07:24 +0530

    new

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run, debug, template
+from bottle import route, run, debug, template, request
 
 @route('/todo')
 @route('/todo-new-page')
@@ -10,5 +10,17 @@ def todo_list():
     result = c.fetchall()
     c.close()
     return template('make_table', rows=result)
+
+@route('/new', method = 'GET')
+def new_item():
+    new = request.GET.task.strip()
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
+    new_id = c.lastrowid
+    conn.commit()
+    c.close()
+    return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id 
+
 debug(True)
 run(reloader=True)
